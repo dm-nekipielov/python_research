fd9350d07c7fa774c5caa9a827d7befe8a4df171
dmnekipelov <dm.nekipelov@gmail.com>
2021-12-14 15:09:37 +0200

    Homework #4 with another way to check the string format.

--- HomeWork.py
+++ HomeWork.py
@@ -1,29 +1,25 @@
 from PlatesList import plates_list
-import re
+from plate_number_processor import get_number_sum, chek_plate_number
 
-
-
-# Надо определить сколько номерных знаков в этом списке уникальны
-# Надо узнать есть ли введенный с клавиатуры номер в списке (без учета регистра букв)
-# Надо узнать сумму всех чисел во введенном номере
-
+print('Type "exit" to quit the program.')
 unique_numbers = set(plates_list)
+
 
 while True:
     plate_number = input('Input interested plate number: ').upper()
-    if bool(re.fullmatch(r"^[A-Z]{2}\d{4}[A-Z]{2}$", plate_number)):
+    if chek_plate_number(plate_number):
+        number_sum = get_number_sum(plate_number[2:6])
         if plate_number in plates_list:
             print(f'Plate number {plate_number} in the "Plate list": FOUND')
         else:
             print(f'Plate number {plate_number} in the "Plate list": NOT FOUND')
+        print(f'The sum of digits: {number_sum}\n'
+              f'Unique plates in the "Plate list": {len(unique_numbers)}')
+    elif plate_number == 'EXIT':
+        print('Bye, bye!')
         break
     else:
         print(f'Invalid input! Please try again.')
 
-number_sum = 0
-for i in range(len(plate_number)):
-    if plate_number[i].isdigit():
-        number_sum += int(plate_number[i])
 
-print(f'The sum of digits: {number_sum}')
-print(f'Unique plates in the "Plate list": {len(unique_numbers)}')
+
--- plate_number_processor.py
+++ plate_number_processor.py
@@ -1,16 +1,26 @@
-from PlatesList import plates_list
-
-while True:
-    plate_number = input("Please enter your desired plate No: ").upper()
-    rules = [len(plate_number) == 8,
-             plate_number[0: 2].isalpha(),
-             plate_number[2: 6].isdigit(),
-             plate_number[-2:].isalpha()]
-    # print(rules)
+def chek_plate_number(value: str):
+    '''
+    The function verified a string against a format.\n
+    :param value: string
+    :return: Return a tuple of values
+    '''
+    rules = [len(value) == 8,
+             value[:2].isalpha(),
+             value[2: 6].isdigit(),
+             value[-2:].isalpha()]
     if not all(rules):
-        print(f'Invalid input! Please try again.')
-        continue
-    elif plate_number in plates_list:
-        print(f'Plate number {plate_number} in the "Plate list": FOUND')
+        return False
     else:
-        print(f'Plate number {plate_number} in the "Plate list": NOT FOUND')
+        items = (value[:2], value[2: 6], value[-2:])
+        return tuple([items[0]] + list(items[1]) + [items[2]])
+
+
+def get_number_sum(value):
+    '''
+    :param value: should be an iterable object
+    :return: Return the sum of values iterable object.
+    '''
+    value_sum = 0
+    for i in value:
+        value_sum += int(i)
+    return value_sum
